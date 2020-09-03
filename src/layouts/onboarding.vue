4fabb05b311e200108fa7aeb1f6f787de641ec19
Nhã <nhalct@fireapps.vn>
2020-09-03 17:07:14 +0700

    add layout for onboarding

--- src/layouts/onboarding.vue
+++ src/layouts/onboarding.vue
@@ -14,5 +14,10 @@ export default {
 
 <style lang="scss">
 .onboarding-layout {
+    min-height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    background-size: 100% 100%;
 }
 </style>
