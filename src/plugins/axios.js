dd0d3677637ccc076152fc11af4ddd902e652a7f
Nhã <nhalct@fireapps.vn>
2020-09-09 22:24:23 +0700

    remove selected book data when token expired

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -44,6 +44,7 @@ axios.interceptors.response.use(
 			header.resultCode == 10413
 		) {
 			cookie.remove(`${token_name}`);
+			cookie.remove(`selected_book`);
 			router.push({ name: routerName.LOGIN });
 			// const refresh_token_data = await api.refresh_token();
 			// const { header, data } = refresh_token_data.data;
