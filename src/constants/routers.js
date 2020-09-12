4d4bd368b0bbfbf5e2caf2437a5d09395fc66b3f
Nhã <nhalct@fireapps.vn>
2020-09-12 23:31:49 +0700

    add layout for transaction children route

--- src/constants/routers.js
+++ src/constants/routers.js
@@ -2,6 +2,7 @@ export default {
     LOGIN: 'Login', 
     DASHBOARD: 'Dashboard', 
     TRANSACTIONS: 'Transactions', 
+    CREATE_TRANSACTION: 'Create Transaction', 
     ONBOARDING: 'Onboarding',
     BOOK: 'Book',
 }
